67223cbfd6129cf8dc470f7d5a7ef72519c9eda9
Simon Wistow <simon@thegestalt.org>
2017-07-28 16:46:19 -0700

    Move to an Actor style model using a ConnectionPool

--- main.go
+++ main.go
@@ -32,17 +32,86 @@ func main() {
 	}
 }
 
-func runServer(address string) error {
-	var connections = map[string]net.Conn{}
-	defer func() {
-		for _, conn := range connections {
-			conn.Close()
+type ConnectionPool struct {
+	connections map[string]net.Conn
+	connectc    chan net.Conn
+	disconnectc chan net.Conn
+	broadcastc  chan Message
+	quitc       chan struct{}
+}
+
+type Message struct {
+	connection net.Conn
+	message    string
+}
+
+func newConnectionPool() *ConnectionPool {
+	return &ConnectionPool{
+		connections: map[string]net.Conn{},
+		connectc:    make(chan net.Conn),
+		disconnectc: make(chan net.Conn),
+		broadcastc:  make(chan Message),
+		quitc:       make(chan struct{}),
+	}
+}
+
+func (pool *ConnectionPool) Add(conn net.Conn) {
+	pool.connectc <- conn
+}
+
+func (pool *ConnectionPool) Delete(conn net.Conn) {
+	pool.disconnectc <- conn
+}
+
+func (pool *ConnectionPool) Broadcast(conn net.Conn, message string) {
+	pool.broadcastc <- Message{connection: conn, message: message}
+}
+
+func (pool *ConnectionPool) Shutdown() {
+	close(pool.quitc)
+}
+
+func (pool *ConnectionPool) Run() {
+	for {
+		select {
+		case conn := <-pool.connectc:
+			name := conn.RemoteAddr().String()
+			log.Printf("Client %s: connected", name)
+			pool.connections[name] = conn
+		case conn := <-pool.disconnectc:
+			name := conn.RemoteAddr().String()
+			log.Printf("Client %s: disconnected", name)
+			delete(pool.connections, name)
+		case message := <-pool.broadcastc:
+			name := message.connection.RemoteAddr().String()
+			text := message.message
+			log.Printf("Client %s: \"%s\"", name, text)
+			for n, c := range pool.connections {
+				if n == name {
+					fmt.Fprintf(c, "> %s\n", text)
+				} else {
+					fmt.Fprintf(c, "%s> %s\n", name, text)
+				}
+			}
+		case <-pool.quitc:
+			for _, conn := range pool.connections {
+				conn.Close()
+			}
+			return
 		}
-	}()
+	}
+
+}
+
+func runServer(address string) error {
+	var err error
+	var pool = newConnectionPool()
+	defer pool.Shutdown()
+	go pool.Run()
 
 	ln, err := net.Listen("tcp", address)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	defer ln.Close()
 	log.Printf("Listening on %s", address)
@@ -53,31 +122,17 @@ func runServer(address string) error {
 			break
 		}
 		go func() {
-			name := conn.RemoteAddr().String()
-			log.Printf("Client %s: connected", name)
-			defer log.Printf("Client %s: disconnected", name)
-			connections[name] = conn
-			defer delete(connections, name)
-			handle(name, conn, connections)
+			pool.Add(conn)
+			defer pool.Delete(conn)
+			scanner := bufio.NewScanner(conn)
+			for scanner.Scan() {
+				text := scanner.Text()
+				pool.Broadcast(conn, text)
+			}
 		}()
 	}
 
 	return err
-}
-
-func handle(name string, conn net.Conn, connections map[string]net.Conn) {
-	scanner := bufio.NewScanner(conn)
-	for scanner.Scan() {
-		text := scanner.Text()
-		log.Printf("Client %s: \"%s\"", name, text)
-		for n, c := range connections {
-			if n == name {
-				fmt.Fprintf(c, "> %s\n", text)
-			} else {
-				fmt.Fprintf(c, "%s> %s\n", name, text)
-			}
-		}
-	}
 }
 
 func runClient(address string) error {
